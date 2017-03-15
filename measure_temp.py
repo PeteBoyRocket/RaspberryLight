5b386b4e231879b28b3d44760db302cfe5ce1ed7
PeteBoyRocket <peteboyrocket@hotmail.com>
2017-03-15 20:09:38 +0000

    updates

---
+++ measure_temp.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import dht11
+
+TempHumidityPin = 7
+
+GPIO.setmode(GPIO.BOARD)
+
+instance = dht11.DHT11(TempHumidityPin)
+
+result = instance.read()
+if result.is_valid():
+    print("Last valid input: " + str(datetime.datetime.now()))
+    print("Temperature: %d C" % result.temperature)
+    print("Humidity: %d %%" % result.humidity)
+    return result.temperature
