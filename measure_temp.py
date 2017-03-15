148a8681f45ee82b5b15378528f7916c75d6a3e8
PeteBoyRocket <peteboyrocket@hotmail.com>
2017-03-15 20:23:45 +0000

    tweak

--- measure_temp.py
+++ measure_temp.py
@@ -9,7 +9,7 @@ instance = dht11.DHT11(TempHumidityPin)
 
 result = instance.read()
 if result.is_valid():
-    print("Last valid input: " + str(datetime.datetime.now()))
-    print("Temperature: %d C" % result.temperature)
-    print("Humidity: %d %%" % result.humidity)
-    return result.temperature
+  #  print("Last valid input: " + str(datetime.datetime.now()))
+  #  print("Temperature: %d C" % result.temperature)
+ #   print("Humidity: %d %%" % result.humidity)
+    print result.temperature
